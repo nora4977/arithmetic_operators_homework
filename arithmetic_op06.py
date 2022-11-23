d6f0addba5329fc648b684e8ad2872026622a0b4
nora4977 <nora4977@nodirayoqubjnova9@gmail.com>
2022-11-23 11:01:44 +0500

    00006 done

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,10 +1,11 @@
 #Create a variable called 'number' with data type int.
-number=27
+number=27.0
 #Divide the number by 2 and assign number.
 number1=number/2
 #Find the fraction of the number and assing to a variable called 'answer'.
-answer=number1%number
+answer=(number,number1)
 #Print the answer.
 print(answer)
 #Print the number
 print(number)
+
