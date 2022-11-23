26013950eedbc567e2030028d1c7a4ec3a57eba8
nora4977 <nora4977@nodirayoqubjnova9@gmail.com>
2022-11-23 08:40:24 +0500

    03 done

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number=24
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer=42
 #Print the answer variable
+print(answer)
