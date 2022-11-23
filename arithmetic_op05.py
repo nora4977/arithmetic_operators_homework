b33461aa9f1f67fa4976c0748f0e6305040652b5
nora4977 <nora4977@nodirayoqubjnova9@gmail.com>
2022-11-23 08:47:13 +0500

    05 done

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,7 +1,7 @@
 #Create a variable called 'number' with data type int.
- 
+number=2211
 #Create a variable called 'answer'
-
+answer=number/3
 #Find the remainder of the division of number by 3.
- 
+print(answer)
 #Print the answer
